514de81f60dcec80eca1344170812beccbd915aa
Rejaul Karim Tirtho <rktirtho@gmail.com>
2021-06-28 12:23:14 +0600

    http basic security added

--- src/main/java/com/rktirthoh/onestore/config/SecurityConfiguration.java
+++ src/main/java/com/rktirthoh/onestore/config/SecurityConfiguration.java
@@ -1,4 +1,48 @@
 package com.rktirthoh.onestore.config;
 
-public class SecurityConfiguration {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("rktirtho").password(passwordEncoder().encode("qwert")).roles("ADMIN")
+                .and()
+                .withUser("robi").password(passwordEncoder().encode("qwert")).roles("MANAGEMENT")
+                .and()
+                .withUser("user").password(passwordEncoder().encode("qwert")).roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+        .and()
+        .httpBasic();
+
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
